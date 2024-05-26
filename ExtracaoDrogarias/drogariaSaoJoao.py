c97dffe50337553057dee4dc7651b25435428954
David Aguina <aguinadavid@gmail.com>
2024-05-26 19:02:06 -0300

    Adiciona interface para Farmacias Sao Joao

--- ExtracaoDrogarias/drogariaSaoJoao.py
+++ ExtracaoDrogarias/drogariaSaoJoao.py
@@ -4,9 +4,14 @@ import time
 import json
 import re
 from math import ceil
+from ExtracaoDrogarias.funcoesAuxiliares import retornaTempoDeExecucaoFormatado, limpaTela, jsonToXlsx
 
-inicio = time.time()
-from funcoesAuxiliares import retornaTempoDeExecucaoFormatado, limpaTela, jsonToXlsx
+if __name__ != "__main__":
+    from ExtracaoDrogarias.funcoesAuxiliares import retornaTempoDeExecucaoFormatado, limpaTela, verificaQuantidadeItens
+    import streamlit as st
+
+caminhoArquivoJSON = "ExtracaoDrogarias/dadosSaoJoaoFarmacia/dadosJSON/saoJoaoFarmacia.json"
+caminhoArquivoXLSX = "ExtracaoDrogarias/dadosSaoJoaoFarmacia/dadosXLSX/saoJoaoFarmacia.xlsx"
 
 # Função para acessar url mesmo em caso de demora de resposta.
 def acessaURL(url):
@@ -32,10 +37,9 @@ def coletaCategorias():
     soup = BeautifulSoup(textoHtml, 'lxml')
     categoriasRegex = r'"category-2","selected":false,"value":"([^"]+)","link"'
     categorias = re.findall(categoriasRegex, str(soup))
-    categorias.remove('fitoterapicos')
+    # Antes a categoria fitoterápicos estava dando problema por causa de erro no site, agora eles arrumaram.
+    # categorias.remove('fitoterapicos')
     return categorias
-
-categorias = coletaCategorias()
 
 # Função para acessar a categoria e coletar o número de produtos total
 def coletaNumeroDeProdutos(categoria):
@@ -46,65 +50,78 @@ def coletaNumeroDeProdutos(categoria):
     numeroDeProdutos = int(re.findall(numeroProdutosRegex, str(soup))[0])
     return numeroDeProdutos
 
-dados = []
-caminhoArquivoJSON = "dadosSaoJoaoFarmacia/dadosJSON/saoJoaoFarmacia.json"
-caminhoArquivoXLSX = "dadosSaoJoaoFarmacia/dadosXLSX/saoJoaoFarmacia.xlsx"
-categoriaExtraida = 0
+# Função principal de extração de dados
+def extracaoDeDados():
+    dadosNovos = []
+    with open(caminhoArquivoJSON, 'w') as file:
+        json.dump(dadosNovos, file, indent=4)
+    inicio = time.time()
+    categoriaExtraida = 0
+    statusExtracao = st.empty()
+    statusCategoriaExtraida = st.empty()
+    categorias = coletaCategorias()
 
-with open(caminhoArquivoJSON, 'w') as file:
-            json.dump(dados, file, indent=4)
-
-def extracaoDeDados(categoria):
-    numeroDeProdutos = coletaNumeroDeProdutos(categoria)
-    numeroDePaginas = ceil(numeroDeProdutos / 24)
-    for pagina in range(1, numeroDePaginas+1):
-        textoHtml = requests.get(f'https://www.saojoaofarmacias.com.br/medicamentos/{categoria}?initialMap=c&initialQuery=medicamentos&map=category-1,category-2&page={pagina}').text
-        nomeRegex = r'"productName":"([^"]+)"'
-        eanRegex = r'"ean":"(\d{0,14})"'
-        marcaRegex = r'"brand":"([^"]+)"'
-        precoRegex = r'"Price":\s*(\d+\.\d+|\d+)'
-        precoSemDescontoRegex = r'"ListPrice":\s*(\d+\.\d+|\d+)'
-        nomes = re.findall(nomeRegex, textoHtml)
-        eans = re.findall(eanRegex, textoHtml)
-        marcas = re.findall(marcaRegex, textoHtml)
-        precos = re.findall(precoRegex, textoHtml)
-        precosSemDesconto = re.findall(precoSemDescontoRegex, textoHtml)
-        descontos = []
-        dados = []
-        for i in range(len(precosSemDesconto)):
-            precos[i] = float(precos[i])
-            precosSemDesconto[i] = float(precosSemDesconto[i])
-            desconto = (1 - (precos[i]/precosSemDesconto[i]))
-            descontos.append(round(round(desconto, 2)*100))
-        for i in range(len(nomes)):
-            dados.append({
-                "Nome do produto": nomes[i],
-                "Preco unitario": precosSemDesconto[i],
-                "Preco venda": precos[i],
-                "Desconto": descontos[i],
-                "EAN": eans[i],
-                "Marca": marcas[i],})
-        try:
-            with open(caminhoArquivoJSON, 'r') as file:
-                dadosGravados = json.load(file)
-            dadosGravados.extend(dados)
-            with open(caminhoArquivoJSON, 'w') as file:
-                json.dump(dadosGravados, file, indent=4)
-        except:
-            with open(caminhoArquivoJSON, 'w') as file:
-                json.dump(dados, file, indent=4)
-        limpaTela()
-        print(f"Página {pagina} de {numeroDePaginas} de {categoria}")
-        print(f"Categoria {categoriaExtraida} de {len(categorias)}")
-
-for i in range(len(categorias)):
-    categoriaExtraida += 1
-    extracaoDeDados(categorias[i])
-
-jsonToXlsx(caminhoArquivoJSON, caminhoArquivoXLSX)
+    for j in range(len(categorias)):
+        categoriaExtraida += 1
+        numeroDeProdutos = coletaNumeroDeProdutos(categorias[j])
+        numeroDePaginas = ceil(numeroDeProdutos / 24)
+        for pagina in range(1, numeroDePaginas+1):
+            textoHtml = requests.get(f'https://www.saojoaofarmacias.com.br/medicamentos/{categorias[j]}?initialMap=c&initialQuery=medicamentos&map=category-1,category-2&page={pagina}').text
+            nomeRegex = r'"productName":"([^"]+)"'
+            eanRegex = r'"ean":"(\d{0,14})"'
+            marcaRegex = r'"brand":"([^"]+)"'
+            precoRegex = r'"Price":\s*(\d+\.\d+|\d+)'
+            precoSemDescontoRegex = r'"ListPrice":\s*(\d+\.\d+|\d+)'
+            nomes = re.findall(nomeRegex, textoHtml)
+            eans = re.findall(eanRegex, textoHtml)
+            marcas = re.findall(marcaRegex, textoHtml)
+            precos = re.findall(precoRegex, textoHtml)
+            precosSemDesconto = re.findall(precoSemDescontoRegex, textoHtml)
+            descontos = []
+            dados = []
+            for i in range(len(precosSemDesconto)):
+                precos[i] = float(precos[i])
+                precosSemDesconto[i] = float(precosSemDesconto[i])
+                desconto = (1 - (precos[i]/precosSemDesconto[i]))
+                descontos.append(round(round(desconto, 2)*100))
+            for i in range(len(nomes)):
+                dados.append({
+                    "Nome do produto": nomes[i],
+                    "Preco unitario": precosSemDesconto[i],
+                    "Preco venda": precos[i],
+                    "Desconto": descontos[i],
+                    "EAN": eans[i],
+                    "Marca": marcas[i],})
+            try:
+                with open(caminhoArquivoJSON, 'r') as file:
+                    dadosGravados = json.load(file)
+                dadosGravados.extend(dados)
+                with open(caminhoArquivoJSON, 'w') as file:
+                    json.dump(dadosGravados, file, indent=4)
+            except:
+                with open(caminhoArquivoJSON, 'w') as file:
+                    json.dump(dados, file, indent=4)
+            limpaTela()
+            print(f"Página {pagina} de {numeroDePaginas} de {categorias[j]}")
+            print(f"Categoria {categoriaExtraida} de {len(categorias)}")
+            if __name__ != "__main__":
+                statusExtracao.write(f"Página {pagina} de {numeroDePaginas} de {categorias[j]}")
+                statusCategoriaExtraida.write(f"Categoria {categoriaExtraida} de {len(categorias)}")
 
 
-fim = time.time()
-tempoExecucao = fim - inicio
-tempoExecucaoFormatado = retornaTempoDeExecucaoFormatado(tempoExecucao=tempoExecucao)
-print(tempoExecucaoFormatado)
+    jsonToXlsx(caminhoArquivoJSON, caminhoArquivoXLSX)
+
+    fim = time.time()
+    tempoExecucao = fim - inicio
+    tempoExecucaoFormatado = retornaTempoDeExecucaoFormatado(tempoExecucao=tempoExecucao)
+    print(tempoExecucaoFormatado)
+
+    if __name__ != "__main__":
+        st.success("Extração finalizada, os dados extraídos já estão atualizados na área de visualização!", icon="✅")
+        with st.expander("Estatísticas sobre a extração"):
+            st.write(f"- {verificaQuantidadeItens(caminhoArquivoJSON)} produtos")
+            st.write(f"{tempoExecucaoFormatado}")
+
+
+if __name__ == "__main__":
+    extracaoDeDados()
--- pages/Extração.py
+++ pages/Extração.py
@@ -1,22 +1,28 @@
 import streamlit as st
 from ExtracaoDrogarias.funcoesAuxiliares import coletaCategorias, retornaNumeroDePaginas
 from ExtracaoDrogarias.farmaPonte import extracaoDeDados
+from ExtracaoDrogarias import drogariaSaoJoao as sj
 
 st.title("Extração")
 
 farmacia_escolhida = st.selectbox(label="Você gostaria de extrair dados de qual farmácia?", placeholder="Escolha uma farmácia...", options=["Farma Ponte", "Drogaria Minas Mais", "Farmácias São João"], index=None)
 
+# Dialog que abre quando o usuário escolhe a categoria para extração.
 @st.experimental_dialog("Extração")
-def exibeProgresso(categoria):
+def exibeProgressoFP(categoria):
     print(categoria)
     st.write(f"Iniciando extração dos produtos da categoria {categoria}")
     st.write(f"url: https://www.farmaponte.com.br/{categoria}/")
-    # progresso = st.progress(0)
     extracaoDeDados(categoria)
-    
+
+@st.experimental_dialog("Extração")
+def exibeProgressoSJ():
+    st.write(f"Iniciando extração dos produtos da Farmácias São João")
+    st.write(f"url: https://www.saojoaofarmacias.com.br/medicamentos/")
+    sj.extracaoDeDados()
 
 
-
+# Interface para a Farma Ponte
 if farmacia_escolhida == "Farma Ponte":
     if 'categoria' not in st.session_state:
         st.session_state['categoria'] = ''
@@ -36,7 +42,14 @@ if farmacia_escolhida == "Farma Ponte":
     
     for info in st.session_state:
         if info in nomesDasCategorias and st.session_state[f"{info}"] == True:
-            exibeProgresso(info.lower())
+            exibeProgressoFP(info.lower())
 
 
-st.write(st.session_state)
+# Interface para Farmácias São João.
+if farmacia_escolhida == "Farmácias São João":
+    st.write(farmacia_escolhida)
+    st.button(label=f"Iniciar extração", use_container_width=True, key="iniciar")
+    if st.session_state["iniciar"] == True:
+        exibeProgressoSJ()
+
+# st.write(st.session_state)
